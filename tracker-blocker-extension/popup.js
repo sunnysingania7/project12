e73285fdf8e60bde73211dbadce354ebe735855b
sunnysingania7 <sunnysingania7@live.com>
2025-06-16 10:25:24 +0530

    Update popup.js

--- tracker-blocker-extension/popup.js
+++ tracker-blocker-extension/popup.js
@@ -1,3 +1,19 @@
 // popup.js
 
 document.getElementById("status").textContent = "No trackers allowed 🚫";
+
+// Show logs
+chrome.storage.local.get("logs", function (data) {
+    const logs = data.logs || [];
+    const logContainer = document.getElementById("logs");
+
+    if (logs.length === 0) {
+        logContainer.innerHTML = "<p>No trackers blocked yet.</p>";
+    } else {
+        logContainer.innerHTML = "<ul>" + 
+        logs.map(log => 
+            `<li><strong>${log.tracker}</strong><br>on <i>${log.page}</i><br><small>${log.time}</small></li>`
+        ).join("") +
+        "</ul>";
+    }
+})
