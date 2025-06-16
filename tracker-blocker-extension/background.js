cfd71aed031a2b4f10fe92c26272d61ec7189a2a
sunnysingania7 <sunnysingania7@live.com>
2025-06-16 08:49:51 +0530

    Update background.js

--- tracker-blocker-extension/background.js
+++ tracker-blocker-extension/background.js
@@ -1,18 +1,22 @@
 // background.js
 
-const trackerDomains = [
-    "google-analytics.com",
-    "facebook.net",
-    "doubleclick.net",
-    "googletagmanager.com"
-];
+let trackerDomains = [];
+let blockedCount = 0;
 
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        return { cancel: true};
-    },
-    {
-        url: trackerDomains.map(domain => `*://*.${domain}/*`)
-    },
-    ["blocking"]
-);
+// Load tracker list
+fetch('data/trackers.json')
+    .then(response => response.json())
+    .then(data => {
+        trackerDomains = data.map(domain => `*://*.${domain}/*`);
+
+        chrome.webRequest.onBeForeRequest.addListener(
+            function(details) {
+                blockedCount++;
+                chrome.action.setBadgeText({ text: blockedCount.toString() });
+                chrome.action.setBadgeBackgroundColor({ color: "#FF0000" });
+                return { cancel: true };
+            },
+            { urls: trackerDomains },
+            ["blocking"]
+        );
+    });
