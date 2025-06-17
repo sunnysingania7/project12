2c80e14eb4a97731b43d28276b33ac52a4ad964c
sunnysingania7 <sunnysingania7@live.com>
2025-06-17 15:38:34 +0530

    Update popup.js

--- tracker-blocker-extension/popup.js
+++ tracker-blocker-extension/popup.js
@@ -1,19 +1,33 @@
-// popup.js
-
 document.getElementById("status").textContent = "No trackers allowed 🚫";
 
-// Show logs
-chrome.storage.local.get("logs", function (data) {
-    const logs = data.logs || [];
-    const logContainer = document.getElementById("logs");
+function loadLogs() {
+    chrome.storage.local.get("logs", function (data) {
+        const logs = data.logs || [];
+        const logContainer = document.getElementById("logs");
 
-    if (logs.length === 0) {
-        logContainer.innerHTML = "<p>No trackers blocked yet.</p>";
-    } else {
-        logContainer.innerHTML = "<ul>" + 
-        logs.map(log => 
-            `<li><strong>${log.tracker}</strong><br>on <i>${log.page}</i><br><small>${log.time}</small></li>`
-        ).join("") +
-        "</ul>";
-    }
-})
+        if (logs.length === 0) {
+            logContainer.innerHTML = "<p>No trackers blocked yet.</p>";
+        } else {
+            logContainer.innerHTML = "<ul>" +
+                logs.map(log =>
+                    `<li>
+                     <a href="${log.tracker}" target="_blank" rel="noopener noreferrer">${log.tracker}</a><br>
+                    on <i>${log.page}</i>
+                    <small>${log.time}</small>
+                </li>`
+                ).join("") +
+                "</ul>";
+        }
+    });
+}
+
+// Clear logs button 
+document.getElementById("clearBtn").addEventListener("click", () => {
+    chrome.storage.local.set({ logs: [] }, () => {
+        document.getElementById("logs").innerHTML = "<p>No trackers blocked yet.</p>";
+        chrome.action.setBadgeText({ text: "" });
+    });
+});
+
+// Load logs on popup open 
+loadLogs();
